abffde18788e1e36f1aae3161b2550330f455ade
bodhi <charliepapa2@gmail.com>
2019-01-14 00:06:11 +0000

    some changes

--- flink-202/src/test/java/com/github/churtado/flink/AvroTests.java
+++ flink-202/src/test/java/com/github/churtado/flink/AvroTests.java
@@ -1,12 +1,16 @@
 package com.github.churtado.flink;
 
 import com.example.Customer;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.restartstrategy.RestartStrategies;
 import org.apache.flink.formats.avro.registry.confluent.ConfluentRegistryAvroDeserializationSchema;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,8 +24,9 @@ import java.util.Properties;
 public class AvroTests {
 
     private StreamExecutionEnvironment env;
-    private FlinkKafkaConsumer010<Customer> consumer010;
+    private FlinkKafkaConsumer011<Customer> consumer011;
     private DataStreamSource<Customer> customers;
+    private FlinkKafkaProducer011<String> producer011;
 
     @BeforeEach
     public void setup(){
@@ -39,25 +44,53 @@ public class AvroTests {
         config.setProperty("bootstrap.servers", "192.168.1.151:9092");
         config.setProperty("group.id", "customer-consumer-group-v2");
         config.setProperty("zookeeper.connect", "localhost:2181");
-        String schemaRegistryUrl = "http://192.168.1.151:8081";
         config.setProperty("specific.avro.reader", "true");
 
-        consumer010 = new FlinkKafkaConsumer010<Customer>(
+        String schemaRegistryUrl = "http://192.168.1.151:8081";
+
+        consumer011 = new FlinkKafkaConsumer011<Customer>(
                 "customer-flink",
                 ConfluentRegistryAvroDeserializationSchema.forSpecific(Customer.class, schemaRegistryUrl),
                 config);
-        consumer010.setStartFromEarliest();
+        consumer011.setStartFromEarliest();
 
-        customers = env.addSource(consumer010);
+        customers = env.addSource(consumer011);
 
         // setup kafka producer
+//        producer011 = new FlinkKafkaProducer011<>(
+//                "customer-avro-string",
+//                new ConfluentRegistryAvroSerializationSchema(),
+//                config);
     }
 
     @Test
     @DisplayName("Testing consuming avro data from kafka")
-    public void testAvro() throws Exception {
+    public void testConsumeKafkaAvroData() throws Exception {
 
-        customers.print();
+        SingleOutputStreamOperator<String> mapToString = customers
+                .map((MapFunction<Customer, String>) SpecificRecordBase::toString);
+
+        mapToString.print();
+
+        env.execute();
+
+    }
+
+    @Test
+    @DisplayName("Testing consuming avro data from kafka")
+    public void testProduceKafkaAvroData() throws Exception {
+
+        Customer customer = Customer.newBuilder()
+                .setFirstName("Jane")
+                .setLastName("Doe")
+                .setAge(26)
+                .setHeight(185.5f)
+                .setWeight(85.6f)
+                .setPhoneNumber("123-1232-4654")
+                .setEmail("john.doe@email.com")
+                .build();
+
+
         env.execute();
 
     }
